87e0bac41e05f7efc295ee72955388540c082c28
Jonathan <johnypatino99@gmail.com>
2023-09-20 13:03:29 -0400

    initial files + pieces

---
+++ script.js
@@ -0,0 +1,8 @@
+const gameboard = document.querySelector("#gameboard");
+const playerTurn = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+const width = 8;
+
+const startPieces = {
+    
+}
